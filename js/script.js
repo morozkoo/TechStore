4778c164e79889d92cb685d4385f55d72438077b
Ilya <ilyamoroz057@gmail.com>
2026-02-24 20:24:11 +0300

    Initial commit: project structure

--- js/script.js
+++
@@ -1,5 +0,0 @@
-console.log('Страница загружена! Добро пожаловать!');
-const title = document.querySelector('h1');
-title.addEventListener('click', () => {
-  title.style.color = title.style.color === 'blue' ? '#2c3e50' : 'blue';
-});
